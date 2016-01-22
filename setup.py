e3b4084f66c00ad35a5df6fbe841cdbe7e6e2d43
dclark87 <danieljclark87@gmail.com>
2016-01-22 14:15:33 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# AWSUtils/setup.py
+#
+# Author: Daniel Clark, 2016
+
+'''
+This is the setup.py installation script to install the awsutils
+package
+'''
+
+# Import packages
+from distutils.core import setup
+
+# Use disutils' setup function to install the package
+setup(name='AWSUtils',
+      version='0.0.1',
+      description='AWS Python utilities',
+      author='Daniel Clark',
+      author_email='daniel.clark@childmind.org',
+      url='https://github.com/FCP-INDI/AWSUtils',
+      packages=['awsutils'])
